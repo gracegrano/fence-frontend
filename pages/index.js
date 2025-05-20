2beabc0721f0ef81d3ebb6c6c9aa27f9438e0ad9
gracegrano <paim.grace@gmail.com>
2025-05-20 16:41:54 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Fence Estimate Dashboard</h1>
+      <p>Welcome! This is your frontend on Vercel.</p>
+    </div>
+  );
+}
